8969902350561e12872fbf419c2384ab2ebc855a
MartinXPN <mirakyanmartin@gmail.com>
2018-12-31 17:38:36 +0400

    Add TabLayout

--- app/src/main/java/com/example/kar/horoscope_world/Forecast.java
+++ app/src/main/java/com/example/kar/horoscope_world/Forecast.java
@@ -1,10 +1,25 @@
 package com.example.kar.horoscope_world;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Forecast extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private MyPagerAdapter myPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +30,70 @@ public class Forecast extends AppCompatActivity {
         String activity_title = intent.getStringExtra("Title");
         setTitle(activity_title);
 
+        myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(myPagerAdapter);
+        tabLayout.setTabsFromPagerAdapter(myPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+
+    public static class MyFragment extends Fragment {
+
+        private static final java.lang.String ARG_PAGE = "arg_page";
+
+        public MyFragment() {
+
+        }
+
+        public static MyFragment newInstance ( int pageNumber ) {
+            MyFragment myFragment = new MyFragment();
+            Bundle arguments = new Bundle();
+
+            arguments.putInt(ARG_PAGE, pageNumber + 1 );
+            myFragment.setArguments( arguments );
+            return myFragment;
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle arguments = getArguments();
+            int pageNumber = arguments.getInt(ARG_PAGE);
+            TextView myText = new TextView(getActivity());
+            myText.setText("Hello I am the text inside this Fragment " + pageNumber);
+            myText.setGravity(Gravity.CENTER);
+            myText.setTextColor(Color.WHITE);
+            return myText;
+        }
     }
 }
+
+class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Forecast.MyFragment myFragment = Forecast.MyFragment.newInstance(position);
+        return myFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Tab" + ( position+1 );
+    }
+}
+
+
+
